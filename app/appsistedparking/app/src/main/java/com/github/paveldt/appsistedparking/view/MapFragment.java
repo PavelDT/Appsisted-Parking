bc023ea8cf37330a32d311c7c8d0585e4735b3f7
paveldt <paveldt2@gmail.com>
2021-02-07 16:42:05 +0000

    Add basic google-map functionality

---
+++ app/appsistedparking/app/src/main/java/com/github/paveldt/appsistedparking/view/MapFragment.java
@@ -0,0 +1,57 @@
+package com.github.paveldt.appsistedparking.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.paveldt.appsistedparking.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MapFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // initialize view
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        // initialize map fragment
+        final SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.googleMap);
+        // use an asynchronous map
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(final GoogleMap googleMap) {
+
+                // todo remove this
+                // testing onclick for api keey
+                googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+                    @Override
+                    public void onMapClick(LatLng latLng) {
+                        MarkerOptions mo = new MarkerOptions();
+                        mo.position(latLng);
+
+                        googleMap.clear();
+
+                        mo.title("TEST " + latLng.latitude + " " + latLng.longitude);
+
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                        googleMap.addMarker(mo);
+                    }
+                });
+
+            }
+        });
+
+        return view;
+    }
+}
--- app/appsistedparking/app/src/main/java/com/github/paveldt/appsistedparking/view/ParkingActivity.java
+++ app/appsistedparking/app/src/main/java/com/github/paveldt/appsistedparking/view/ParkingActivity.java
@@ -1,6 +1,7 @@
 package com.github.paveldt.appsistedparking.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
@@ -12,5 +13,10 @@ public class ParkingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking);
+
+        // initialize map fragment
+        Fragment mapFragment = new MapFragment();
+        // open the map fragment
+        getSupportFragmentManager().beginTransaction().replace(R.id.mapFrame, mapFragment).commit();
     }
 }
