13e4dcf6cd725c7213a9b89fcd265754c32071b0
paveldt <paveldt2@gmail.com>
2021-02-09 18:22:52 +0000

    Add bound between user location and parking destination, update map to that bound

--- app/appsistedparking/app/src/main/java/com/github/paveldt/appsistedparking/view/MapFragment.java
+++ app/appsistedparking/app/src/main/java/com/github/paveldt/appsistedparking/view/MapFragment.java
@@ -35,7 +35,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment implements LocationListener {
 
-    private LocationManager locationManager;
+
     private Location userLocation;
 
     @Override
@@ -49,9 +49,6 @@ public class MapFragment extends Fragment implements LocationListener {
         // initialize view
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        // todo -- test
-        statusCheck();
-
         // initialize map fragment
         final SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.googleMap);
         // use an asynchronous map
@@ -63,36 +60,68 @@ public class MapFragment extends Fragment implements LocationListener {
                 googleMap.clear();
 
 
-                // lattitude and longtitude of the university
-                LatLng parkingDestination = new LatLng(56.144947260528994, -3.9204526421331267);
-                // user's current lattitude and longtitude
-                // todo
-                // LatLng userPosition = getLocation();
-
-                // add marker for stirling university
-                MarkerOptions stirlingMarkerOpt = new MarkerOptions();
-                stirlingMarkerOpt.position(parkingDestination);
-                Marker stirlingMarker = googleMap.addMarker(stirlingMarkerOpt);
-
-                // todo -- remove this once the bound camera works
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(parkingDestination, 13));
+//                // lattitude and longtitude of the university
+//                LatLng parkingDestination = new LatLng(56.144947260528994, -3.9204526421331267);
+//                // add marker for stirling university
+//                MarkerOptions stirlingMarkerOpt = new MarkerOptions();
+//                stirlingMarkerOpt.position(parkingDestination);
+//                Marker stirlingMarker = googleMap.addMarker(stirlingMarkerOpt);
+//
+//                MarkerOptions userMarkerOpt = new MarkerOptions();
+//                userMarkerOpt.position(new LatLng(userLocation.getLatitude(), userLocation.getLongitude()));
+//                Marker userMarker = googleMap.addMarker(stirlingMarkerOpt);
+//
+//                // todo -- remove this once the bound camera works
+//                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(parkingDestination, 2));
 
 
                 // set camera between current user's position and the parking location
-                LatLngBounds.Builder builder = new LatLngBounds.Builder();
-                // add stirling lat-long
-                builder.include(parkingDestination);
-                // add user's lat-long
-                // todo -- add this back once permissions are sorted
-//                if (userPosition != null) {
-//                    builder.include(userPosition);
-//                }
-//                LatLngBounds bounds = builder.build();
-//                // create a camera update
-//                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 13);
-//                // update the camera
-//                googleMap.moveCamera(cameraUpdate);
-////                stirlingMarker.showInfoWindow();
+
+                // create a camera update
+
+                // update the camera
+
+                googleMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+                    @Override
+                    public void onMapLoaded() {
+                        // create latlons for stirling and user location
+                        LatLng stirlingLatLng = new LatLng(56.144947260528994, -3.9204526421331267);
+                        LatLng userLatLng = new LatLng(userLocation.getLatitude(), userLocation.getLongitude());
+
+                        // add stirling university marker
+                        MarkerOptions stirlingMarkerOpt = new MarkerOptions();
+                        stirlingMarkerOpt.position(stirlingLatLng);
+                        stirlingMarkerOpt.title("Univ. Stirling");
+                        Marker stirlingMarker = googleMap.addMarker(stirlingMarkerOpt);
+
+                        // add user location marker
+                        MarkerOptions userMarkerOpt = new MarkerOptions();
+                        userMarkerOpt.position(userLatLng);
+                        userMarkerOpt.title("Me");
+                        Marker userMarker = googleMap.addMarker(userMarkerOpt);
+
+                        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+                        // add stirling lat-long
+                        builder.include(stirlingLatLng);
+                        builder.include(userLatLng);
+                        LatLngBounds bounds = builder.build();
+
+
+                        // updates camera position to include the user's starting position and
+                        // stirling university parking. The padding ensures both locations are visible
+                        int padding = 200;
+                        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+                        googleMap.moveCamera(cameraUpdate);
+
+                        // todo -- currently doesn't do anything as there is no title etc added
+                        // display marker info on the map
+                        userMarker.showInfoWindow();
+                        stirlingMarker.showInfoWindow();
+                    }
+                });
+
+
+//                stirlingMarker.showInfoWindow();
 
                 // todo remove this
                 // testing onclick for api keey
@@ -117,66 +146,71 @@ public class MapFragment extends Fragment implements LocationListener {
         return view;
     }
 
-    // based on https://stackoverflow.com/questions/32290045
-    protected LatLng getLocation() {
-
-        if (checkLocationPermission()) {
-
-            locationManager = (LocationManager) this.getContext().getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            String bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true));
-
-            //You can still do this if you like, you might get lucky:
-            Location location = locationManager.getLastKnownLocation(bestProvider);
-            locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
-            double lat = location.getLatitude();
-            double lon = location.getLongitude();
-            return new LatLng(lat, lon);
-        } else {
-            return null;
-        }
+    public void setUserLocation(Location locUpdate) {
+        userLocation = locUpdate;
     }
 
-    private boolean checkLocationPermission() {
-        Log.i("REEE", ""  + (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)));
-        Log.i("REEE", ""  + (PackageManager.PERMISSION_GRANTED));
-
-
-        if(ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-           return true;
-        } else {
-            Log.i("AAAAAADFASDf", "bb");
-            String[] permissions = new String[]{"android.permission.ACCESS_FINE_LOCATION"};
-            ActivityCompat.requestPermissions(this.getActivity(), permissions, 0);
-        }
-
-        return ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
-    }
-
-    public void statusCheck() {
-        final LocationManager manager = (LocationManager) this.getActivity().getSystemService(Context.LOCATION_SERVICE);
-
-        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            buildAlertMessageNoGps();
-
-        }
-    }
-
-    private void buildAlertMessageNoGps() {
-        final AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext());
-        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
-                .setCancelable(false)
-                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, final int id) {
-                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-                    }
-                })
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    public void onClick(final DialogInterface dialog, final int id) {
-                        dialog.cancel();
-                    }
-                });
-        final AlertDialog alert = builder.create();
-        alert.show();
-    }
+//    // based on https://stackoverflow.com/questions/32290045
+//    private LocationManager locationManager;
+//    protected LatLng getLocation() {
+//
+//        if (checkLocationPermission()) {
+//
+//            locationManager = (LocationManager) this.getContext().getSystemService(Context.LOCATION_SERVICE);
+//            Criteria criteria = new Criteria();
+//            String bestProvider = String.valueOf(locationManager.getBestProvider(criteria, true));
+//
+//            //You can still do this if you like, you might get lucky:
+//            Location location = locationManager.getLastKnownLocation(bestProvider);
+//            locationManager.requestLocationUpdates(bestProvider, 1000, 0, this);
+//            double lat = location.getLatitude();
+//            double lon = location.getLongitude();
+//            return new LatLng(lat, lon);
+//        } else {
+//            return null;
+//        }
+//    }
+//
+//    private boolean checkLocationPermission() {
+//        Log.i("REEE", ""  + (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)));
+//        Log.i("REEE", ""  + (PackageManager.PERMISSION_GRANTED));
+//
+//
+//        if(ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//           return true;
+//        } else {
+//            Log.i("AAAAAADFASDf", "bb");
+//            String[] permissions = new String[]{"android.permission.ACCESS_FINE_LOCATION"};
+//            ActivityCompat.requestPermissions(this.getActivity(), permissions, 0);
+//        }
+//
+//        return ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+//    }
+//
+//    public void statusCheck() {
+//        final LocationManager manager = (LocationManager) this.getActivity().getSystemService(Context.LOCATION_SERVICE);
+//
+//        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+//            buildAlertMessageNoGps();
+//
+//        }
+//    }
+//
+//    private void buildAlertMessageNoGps() {
+//        final AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext());
+//        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+//                .setCancelable(false)
+//                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                    public void onClick(final DialogInterface dialog, final int id) {
+//                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+//                    }
+//                })
+//                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                    public void onClick(final DialogInterface dialog, final int id) {
+//                        dialog.cancel();
+//                    }
+//                });
+//        final AlertDialog alert = builder.create();
+//        alert.show();
+//    }
 }
--- app/appsistedparking/app/src/main/java/com/github/paveldt/appsistedparking/view/ParkingActivity.java
+++ app/appsistedparking/app/src/main/java/com/github/paveldt/appsistedparking/view/ParkingActivity.java
@@ -1,38 +1,41 @@
 package com.github.paveldt.appsistedparking.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.github.paveldt.appsistedparking.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class ParkingActivity extends AppCompatActivity {
+
+    MapFragment mapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking);
 
-        // initialize map fragment
-        Fragment mapFragment = new MapFragment();
-        // open the map fragment
-        getSupportFragmentManager().beginTransaction().replace(R.id.mapFrame, mapFragment).commit();
+        // user location and map fragment initialisation
+        initMapFragment();
 
         // initialize controls
         initLogoutButton();
-
-
-        Log.i("REEEff", "" + (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ));
-        Log.i("REEEff", ""  + (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED));
-
     }
 
     private void initLogoutButton() {
@@ -45,5 +48,43 @@ public class ParkingActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void initMapFragment() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // wasteful but necessary, creates a client to access location services
+            FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            // requests last known location and triggers a callback once the location is found
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+                    if (location != null) {
+                        Log.i("WIN!!!", location.toString());
+
+                        // initialize map fragment
+                        mapFragment = new MapFragment();
+                        mapFragment.setUserLocation(location);
+
+                        // open the map fragment
+                        getSupportFragmentManager().beginTransaction().replace(R.id.mapFrame, mapFragment).commit();
+
+                    } else {
+                        // nothing to do, location is inaccessible
+                        // this can happen on virtual devices or if location services are entirely disabled.
+                        Toast.makeText(ParkingActivity.this, "Error fetching location.", Toast.LENGTH_LONG);
+                    }
+                }
+            });
+        } else {
+            // request permission
+            ActivityCompat.requestPermissions(ParkingActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+    }
+
+    private boolean locationPermissionCheck() {
+
+
+        return false;
     }
 }
