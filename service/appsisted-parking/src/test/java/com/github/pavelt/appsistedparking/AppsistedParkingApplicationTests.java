6f6ca8058d227a2ca7720113cdceb0a0bcef9582
paveldt <paveldt2@gmail.com>
2021-03-14 15:21:27 +0000

    Add test suite to manage tests, start webservice once per test run

--- service/appsisted-parking/src/test/java/com/github/pavelt/appsistedparking/AppsistedParkingApplicationTests.java
+++ service/appsisted-parking/src/test/java/com/github/pavelt/appsistedparking/AppsistedParkingApplicationTests.java
@@ -1,13 +1,30 @@
 package com.github.pavelt.appsistedparking;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class AppsistedParkingApplicationTests {
+import java.io.IOException;
+import java.net.ServerSocket;
+
+@RunWith(SpringRunner.class)
+public class AppsistedParkingApplicationTests {
+
+	@Before
+	@Test
+	public void contextLoads() {
+	}
 
 	@Test
-	void contextLoads() {
+	public void testServiceUp() throws IOException {
+		try (ServerSocket ignored = new ServerSocket(8080)) {
+			// success, test can pass.
+			throw new RuntimeException("Port not in use - service isn't running.");
+		} catch (IOException e) {
+			// all good, port is in use, service should be running.
+		}
 	}
 
 }
---
+++ service/appsisted-parking/src/test/java/com/github/pavelt/appsistedparking/TestSuite.java
@@ -0,0 +1,45 @@
+package com.github.pavelt.appsistedparking;
+
+
+import com.github.pavelt.appsistedparking.security.PasswordManagerTest;
+import com.github.pavelt.appsistedparking.security.SanitizerTest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        AppsistedParkingApplicationTests.class,
+        PasswordManagerTest.class,
+        SanitizerTest.class
+})
+public class TestSuite {
+
+    // context used to stop service once it's finished being tested.
+    private static ConfigurableApplicationContext cap;
+
+    /**
+     * Start database and web-service used for testing.
+     */
+    @BeforeClass
+    public static void setup() {
+
+        // start Cassandra
+
+        // start web service
+        cap = SpringApplication.run(AppsistedParkingApplication.class, "");
+    }
+
+    /**
+     * Stop database and web-service
+     */
+    @AfterClass
+    public static void stop() {
+        cap.stop();
+    }
+
+}
